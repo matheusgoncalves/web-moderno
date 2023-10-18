f8aa22b804a4786f21d6fdc40e264922cd63c41d
Matheus Gonçalves <matheusgoncalves2@icloud.com>
2023-10-18 04:33:21 -0300

    first commit

---
+++ exercicios-js/fundamentos/numeroAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0); // Infinity
+console.log("10" / 2); // 5
+console.log("3" + 2); // 32 -> string ganha!
+console.log("3" - 2); // 1
+console.log("Show!" * 2); // NaN
+console.log(0.1 + 0.7); // 0.7999999999999999
+// console.log(10.toString()); // SyntaxError: Invalid or unexpected token
+console.log((10.345).toFixed(2)); // 10.35
